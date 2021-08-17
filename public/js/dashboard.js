96a05291f498d9759c71312ceccaa31d4989af86
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-17 15:27:28 +0700

    update dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -94,7 +94,7 @@ window.setTimeout("waktu()", 1000);
 	}
 
 // Count Down Idul Adha
-var countDownDate = new Date("July 19, 2021 00:00:00").getTime();
+var countDownDate = new Date("Januari 01, 2021 00:00:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
@@ -109,7 +109,7 @@ var x = setInterval(function() {
   + minutes + " Min " + seconds + " Sec ";
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("IdulAdha").innerHTML = "Selamat Hari Raya Idul Adha ";
+    document.getElementById("IdulAdha").innerHTML = "Selamat Tahun Baru✨ ";
   }
 }, 1000);
 
