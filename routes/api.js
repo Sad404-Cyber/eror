86a1c835047f9a8b2024636eee0f6b36b52a1520
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-19 03:20:32 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -900,7 +900,7 @@ router.get('/maker/ttp', async(req, res, next) => {
   data = await fetch(hasil).then(v => v.getBuffer())
   await fs.writeFileSync(__path +'/tmp/attp.gif', data)
   res.sendFile(__path +'/tmp/attp.gif')
-  
+          
          .catch(e => {
          	res.json(loghandler.error)
       
