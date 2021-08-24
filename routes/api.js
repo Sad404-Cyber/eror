dad003b421863b2bbcaf4d5a6afb286869d4f43c
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-25 03:01:56 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -16,19 +16,25 @@ var creator = creatorList[Math.floor(Math.random() * creatorList.length)];
 var key = 'EkakeY' // Apikey Lu Ngab
 
 var ffmpeg = require('fluent-ffmpeg');
-var upload = require(__path + '/lib/upload.js');
+var brainly = require('brainly-scraper-v2');
 var imageToBase64 = require('image-to-base64');
+var upload = require(__path + '/lib/upload.js');
 var axios = require('axios');
 var FormData = require('form-data');
 var ytdl = require('ytdl-core');
 var ytpl = require('ytpl');
+var qrcode = require('qrcode');
 var secure = require('ssl-express-www');
 var cors = require('cors');
-var scrapeYt = require("scrape-yt");
+var scrapeYt = require('scrape-yt');
+var gtts = require('node-gtts');
 var fetch = require('node-fetch');
 var cheerio = require('cheerio');
 var request = require('request');
 var TikTokScraper = require('tiktok-scraper');
+var yts = require('yt-search');
+var fs = require('fs');
+var util = require('util');
 var router  = express.Router();
 
 var { tts, wait, simih, getBuffer, h2k, banner, getRandom, start, info, success, close, pickRandom } = require(__path + '/lib/functions.js');
@@ -40,13 +46,7 @@ var { color, bgcolor } = require(__path + '/lib/color.js');
 var { fetchJson } = require(__path + '/lib/fetcher.js')
 var { recognize } = require(__path + '/lib/ocr.js')
 var options = require(__path + '/lib/options.js');
-var {
-	Nulis,
-	Vokal,
-	Base,
-	Searchnabi,
-    Gempa
-} = require('./../lib');
+var { Vokal, Base, Searchnabi, Gempa } = require('./../lib');
 var cookie = "HSID=A7EDzLn3kae2B1Njb;SSID=AheuwUjMojTWvA5GN;APISID=cgfXh13rQbb4zbLP/AlvlPJ2xBJBsykmS_;SAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;__Secure-3PAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;VISITOR_INFO1_LIVE=RgZLnZtCoPU;LOGIN_INFO=AFmmF2swRQIhAOXIXsKVou2azuz-kTsCKpbM9szRExAMUD-OwHYiuB6eAiAyPm4Ag3O9rbma7umBK-AG1zoGqyJinh4ia03csp5Nkw:QUQ3MjNmeXJ0UHFRS3dzaTNGRmlWR2FfMDRxa2NRYTFiN3lfTEdOVTc4QUlwbUI4S2dlVngxSG10N3ZqcHZwTHBKano5SkN2dDlPSkhRMUtReE42TkhYeUVWS3kyUE1jY2I1QzA1MDZBaktwd1llWU9lOWE4NWhoZV92aDkxeE9vMTNlcG1uMU9rYjhOaDZWdno2ZzN3TXl5TVNhSjNBRnJaMExrQXpoa2xzRVUteFNWZDI5S0Fn;PREF=app=desktop&f4=4000000&al=id;SID=2wezCMTUkWN3YS1VmS_DXaEU84J0pZIQdemM8Zry-uzWm8y1njBpLTOpxSfN-EaYCRSiDg.;YSC=HCowA1fmvzo;__Secure-3PSID=2wezCMTUkWN3YS1VmS_DXaEU84J0pZIQdemM8Zry-uzWm8y1dajgWzlBh9TgKapGOwuXfA.;SIDCC=AJi4QfFK0ri9fSfMjMQ4tOJNp6vOb9emETXB_nf2S05mvr2jBlmeEvlSsQSzPMuJl_V0wcbL1r8;__Secure-3PSIDCC=AJi4QfGeWHx-c4uTpU1rXCciO1p0s2fJWU07KrkZhWyD1Tqi8LyR-kHuBwHY9mViVYu1fRh2PA";
 
 loghandler = {
