8afa7965dcc7e6231edf5431795fcb0581de84cf
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-26 03:04:04 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -325,7 +325,9 @@ router.get('/store', async (req, res, next) => {
         var result = data;
              res.json({
              	creator: 'Hafidz Abdillah',
-                 result
+                 code: 200,
+                 message: 'succes',
+                 data: result.result
              })
          })
          .catch(e => {
