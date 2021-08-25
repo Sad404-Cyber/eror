167cce88b26ef54169e80ec303398d61590cdcb8
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-26 03:09:53 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -2323,7 +2323,9 @@ router.get('/grupwa', async (req, res, next) => {
         var result = data;
              res.json({
              	creator: 'Hafidz Abdillah',
-                 result
+                 code: 200,
+                 message: 'succes',
+                 data: result.result
              })
          })
          .catch(e => {
