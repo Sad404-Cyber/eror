356c6a23bed86568e07b4ab207f8aeec6429dfe6
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-26 00:28:52 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -348,7 +348,9 @@ router.get('/mediafire', async (req, res, next) => {
         var result = data;
              res.json({
              	creator: 'Hafidz Abdillah',
-                 result
+                 code: 200,
+                 message: 'succes',
+                 data: result.result
              })
          })
          .catch(e => {
