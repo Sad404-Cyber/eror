b324df1d364bd2b0d20b0307d39337c03247580a
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-26 02:50:31 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -3060,7 +3060,9 @@ router.get('/cuaca', async (req, res, next) => {
         var result = data;
              res.json({
              	creator: 'Hafidz Abdillah',
-                 result
+                 code: 200,
+                 message: 'succes',
+                 data: result.result
              })
          })
          .catch(e => {
