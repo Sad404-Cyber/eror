a008594085dbe3fa85090a2c5e93a1721786ea6f
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-26 02:55:23 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -3088,7 +3088,9 @@ router.get('/hoax', async (req, res, next) => {
         var result = data;
              res.json({
              	creator: 'Hafidz Abdillah',
-                 result
+                 code: 200,
+                 message: 'succes',
+                 data: result.result
              })
          })
          .catch(e => {
