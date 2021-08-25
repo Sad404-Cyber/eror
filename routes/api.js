3c81412a9931764501c9830ea127a5d80099dfad
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-26 01:33:22 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -2427,7 +2427,9 @@ router.get('/stalkyt', async (req, res, next) => {
         var result = data;
              res.json({
              	creator: 'Hafidz Abdillah',
-                 result
+                 code: 200,
+                 message: 'succes',
+                 data: result.result
              })
          })
          .catch(e => {
