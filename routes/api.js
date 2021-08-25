3fddce07b3de1bee3feefe8661e63e5094176ec7
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-26 03:15:05 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -1048,7 +1048,9 @@ router.get('/zodiak', async (req, res, next) => {
         var result = data;
              res.json({
                creator: 'Hafidz Abdillah',
-                 result
+                 code: 200,
+                 message: 'succes',
+                 data: result.result
              })
          })
          .catch(e => {
