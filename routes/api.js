7ce38d0f5ad89efa17433d53785bc76117c421f6
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-26 02:28:21 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -3339,7 +3339,9 @@ router.get('/manga', async (req, res, next) => {
         var result = data;
              res.json({
              	creator: 'Hafidz Abdillah',
-                 result
+                 code: 200,
+                 message: 'succes',
+                 data: result.result
              })
          })
          .catch(e => {
