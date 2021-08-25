6496889e961a981808450d2a34ab837105a5d1fb
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-26 02:12:31 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -3431,8 +3431,10 @@ router.get('/jago', async (req, res, next) => {
         .then(data => {
         var result = data;
              res.json({
-                 creator : 'Hafidz abdillah',
-                 result
+                 creator: 'Hafidz Abdillah',
+                 code: 200,
+                 message: 'succes',
+                 data: result.result
              })
          })
          .catch(e => {
