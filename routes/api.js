90939bf52cbf9cb9663da201079a086103a4a4dd
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-26 14:44:13 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -801,7 +801,9 @@ router.get('/sekolah', async (req, res, next) => {
         var result = data;
              res.json({
              	creator: 'Hafidz Abdillah',
-                 result
+                 code: 200,
+                 message: 'succes',
+                 data: result.result
              })
          })
          .catch(e => {
