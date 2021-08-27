a95cbcd280184cc798171b7e607f5ec63a5f6615
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-28 04:00:44 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -370,6 +370,35 @@ router.get('/store', async (req, res, next) => {
 }
 })
 
+router.get('/katailham', async (req, res, next) => {
+        var apikey = req.query.apikey
+            
+            
+	if(!apikey) return res.json(loghandler.notparam)
+        
+
+       if(listkey.includes(apikey)){
+       fetch(encodeURI(`https://leyscoders-api.herokuapp.com/api/katailham?apikey=dappakntlll`))
+        .then(response => response.json())
+        .then(data => {
+        var result = data;
+             res.json({
+                 creator: 'Hafidz Abdillah',
+                 code: 200,
+                 message: 'succes',
+                 result: {
+                 	hasil: result.result
+                 }
+             })
+         })
+         .catch(e => {
+         	res.json(loghandler.error)
+})
+} else {
+  res.sendFile(invalidKey)
+}
+})
+
 router.get('/nyindir', async (req, res, next) => {
         var apikey = req.query.apikey
             
