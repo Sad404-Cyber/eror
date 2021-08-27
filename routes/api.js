1f2979ddbfeea881645243087f15fd589592c877
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-27 12:50:20 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -1142,7 +1142,9 @@ router.get('/cerpen', async (req, res, next) => {
         var result = data;
              res.json({
                  creator: 'Hafidz Abdillah',
-                 result
+                 code: 200,
+                 message: 'succes',
+                 data: result.result
              })
          })
          .catch(e => {
